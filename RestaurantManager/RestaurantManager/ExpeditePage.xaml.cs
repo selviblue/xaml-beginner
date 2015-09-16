97e737097ad0b34037d99e2810b2e9ba89b015be
selviblue <selvi.palani@aspiresys.com>
2015-09-15 23:09:38 -0700

    Sample code done by Selvi
    Sample code done by Selvi

---
+++ RestaurantManager/RestaurantManager/ExpeditePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using RestaurantManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantManager
+{
+    /// <summary>
+    /// Interaction logic for ExpeditePage.xaml
+    /// </summary>
+    public partial class ExpeditePage : Page
+    {
+        public ExpeditePage()
+        {
+            InitializeComponent();
+            this.Loaded += ExpeditePage_Loaded;
+        }
+
+        void ExpeditePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgGrid.ItemsSource = new DataManager().OrderItems;
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainPage = new MainWindow();
+            NavigationService.GetNavigationService(this).Navigate(mainPage);
+        }
+    }
+}
---
+++ RestaurantManager/RestaurantManager/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Page
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderView_Click(object sender, RoutedEventArgs e)
+        {
+            ExpeditePage expedited = new ExpeditePage();
+            NavigationService.GetNavigationService(this).Navigate(expedited);
+        }
+
+        private void SubmitOrder_Click(object sender, RoutedEventArgs e)
+        {
+            OrderPage order = new OrderPage();
+            NavigationService.GetNavigationService(this).Navigate(order);
+        }
+    }
+}
